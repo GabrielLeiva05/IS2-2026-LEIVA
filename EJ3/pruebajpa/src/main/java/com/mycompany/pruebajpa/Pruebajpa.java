1d3b8f0bc645f93d01e82cdef2c5820729665e8f
Gabriel Leiva <gabrielnleiva2@gmail.com>
2026-08-17 12:22:46 -0300

    Agrego proyecto maven con jpa, con la conexión a la BBDD establecida y clase Alumno como @Entity.

---
+++ EJ3/pruebajpa/src/main/java/com/mycompany/pruebajpa/Pruebajpa.java
@@ -0,0 +1,12 @@
+package com.mycompany.pruebajpa;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Pruebajpa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
