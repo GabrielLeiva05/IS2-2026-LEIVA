2f4db3ee5047fd25a54720779cb806aaa370528c
Gabriel Leiva <gabrielnleiva2@gmail.com>
2026-08-17 00:46:54 -0300

    Agrego ejercicios introductorios del curso de react.

---
+++ EJ2/ejercicio_codi_link_js_vanilla/script.js
@@ -0,0 +1,20 @@
+// Vanilla JavaScript
+
+// recuperamos el boton
+const button = document.querySelector("button")
+
+// al hacer click en el boton, tenemos que ejecutar una funcion
+button.addEventListener("click", function () {
+  //recuperar la id del atributo del HTML
+  const id = button.getAttribute("data-id")
+
+  // llamar a un servicio para actualizar si me gusta
+  // toggleLike(id)
+  if(button.classList.contains("liked")) {
+    button.classList.remove("liked")
+    button.innetText="Me gusta"
+  } else {
+    button.classList.add("liked")
+    button.innerText="Quitar me gusta"
+  }
+})
