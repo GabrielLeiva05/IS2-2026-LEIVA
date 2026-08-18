78e8ec8d5de2e27e41c0b02963eaa49d4b23b0a4
Gabriel Leiva <gabrielnleiva2@gmail.com>
2026-08-17 21:42:11 -0300

    Modifico el archivo de config de la bd.

--- EJ3/j.REACT/GUIA-AUDIOVISUAL/crud-fb-react/src/firebaseConfig/firebase.js
+++ EJ3/j.REACT/GUIA-AUDIOVISUAL/crud-fb-react/src/firebaseConfig/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBU0u6DFfRiOLBZQZgdZWdhbZ37S9KI08",
-  authDomain: "crud-fire-react-b4a3d.firebaseapp.com",
-  projectId: "crud-fire-react-b4a3d",
-  storageBucket: "crud-fire-react-b4a3d.firebasestorage.app",
-  messagingSenderId: "351633322446",
-  appId: "1:351633322446:web:c9719cc2306832c5997b9f",
-  measurementId: "G-PQWENXL264"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
