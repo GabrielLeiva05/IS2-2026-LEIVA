ed4fafa1da6fd08d5ff2426099495f7d8737f7f0
Gabriel Leiva <gabrielnleiva2@gmail.com>
2026-08-25 16:05:39 -0300

    Agrego th en el saludo.html y @RequestParam y Model en SaludoController.java

--- EJ4/g.Thymeleaf/PruebaThymeleaf/src/main/java/com/todocodeacademy/PruebaThymeleaf/controller/SaludoController.java
+++ EJ4/g.Thymeleaf/PruebaThymeleaf/src/main/java/com/todocodeacademy/PruebaThymeleaf/controller/SaludoController.java
@@ -1,7 +1,9 @@
 package com.todocodeacademy.PruebaThymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -14,7 +16,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SaludoController {
     
     @GetMapping("/saludo")
-    public String saludo() {
+    public String saludo(@RequestParam String nombre,
+                          Model model) {
+        model.addAttribute("nombre", nombre);
         return "saludo";
     }
 }
