f4ab56cb1a3de50a77809eec7b901a33ec985d7c
Gabriel Leiva <gabrielnleiva2@gmail.com>
2026-08-28 12:07:43 -0300

    Agrego códigos de ejemplo de investigación de React.

---
+++ EJ5/INVESTIGACION-REACT/investigacion-react/src/App.jsx
@@ -0,0 +1,18 @@
+import Ejemplo1 from './components/Ejemplo1';
+import Ejemplo2 from './components/Ejemplo2';
+
+function App() {
+    return (
+        <main>
+            <h1>Investigación de React</h1>
+
+            <Ejemplo1 />
+
+            <hr />
+
+            <Ejemplo2 />
+        </main>
+    );
+}
+
+export default App;
