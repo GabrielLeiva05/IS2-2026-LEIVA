1036d8755b023375d1a0e4ba5d1b68cb3ebb7ab7
Gabriel Leiva <gabrielnleiva2@gmail.com>
2026-09-15 17:49:02 -0300

    Agrego proyecto de react con vite para ejemplificar jest y react testing library.

---
+++ EJ7/h.INVESTIGACION-REACT/investigacion-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Contador from "./components/Contador";
+
+function App() {
+    return (
+        <div>
+            <h1>Investigación React - EJ7</h1>
+
+            <Contador />
+        </div>
+    );
+}
+
+export default App;
